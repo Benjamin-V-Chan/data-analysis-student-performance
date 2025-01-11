bab72613acd6f456d2e516a22a4eab07d04e4cd2
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-10 23:47:04 -0800

    04 data visualization script outline

---
+++ scripts/04_data_visualization.py
@@ -0,0 +1,8 @@
+# Use seaborn, matplotlib, and pandas
+# Seaborn to perform pairplot
+# plt to save and close figs, as well as adjust sizing
+# Seaborn to also create heatmap using .corr()
+# Visualizations total will be:
+    # Pairplot (SNS)
+    # Corr Heatmap (SNS)
+# Use cleaned_data.csv
